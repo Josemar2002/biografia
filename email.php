844b37a3bf8dfd4d81dc0fd6dd80ed95f5a1b6d8
Josemar Rodrigues <rjosemar008@gmail.com>
2023-10-31 20:10:07 -0300

    test

--- email.php
+++ email.php
@@ -1,55 +1,30 @@
-php
 <?php
-$nome = $_POST['name'];
-$email = $_POST['email'];
-$mensagem = $_POST['message'];
-
-// Defina o endereço de e-mail para onde deseja enviar os dados do formulário
-$to = "christianfelix.timoteo3@gmail.com";
-
-$subject = "Contato - Programador Br";
-
-$body = "Nome: ".$nome."\r\n".
-        "Email: ".$email."\r\n".
-        "Mensagem: ".$mensagem;
-
-// Envie o e-mail
-
-$header = "From: christianfelix.timoteo2@gmail.com"."\r\n"
-            ."Reply-To: ".$email. "\r\n"
-            ."X-Mailer: PHP/".phpversion();
-
-if (mail($to, $subject, $body, $header)){
-    echo ("Obrigado por entrar em contato! Seu formulário foi enviado com sucesso.");
-}else{
-    echo ("Houve um erro ao enviar sua mensagem");
-}
-?>
-<!-- < -->
-// $nome = addslashes($_POST['name']);
-//     $email = addslashes($_POST['email']);
-//     $mensagem = addslashes($_POST['message']); -->
+$nome = addslashes($_POST['name']);
+    $email = addslashes($_POST['email']);
+    $mensagem = addslashes($_POST['message']);
 
     // Defina o endereço de e-mail para onde deseja enviar os dados do formulário
-    // $to = "christianfelix.timoteo3@gmail.com";
+    $to = "christianfelix.timoteo3@gmail.com";
 
-    // $subject = "Contato - Programador Br";
+    $subject = "Contato - Programador Br";
 
-    // $body = "Nome: ".$nome."\r\n".
-    //         "Email: ".$email."\r\n".
-    //         "Mensagem: ".$mensagem;
+    $body = "Nome: ".$nome."\r\n".
+            "Email: ".$email."\r\n".
+            "Mensagem: ".$mensagem;
 
     // Envie o e-mail
 
-    // $header = "From:christianfelix.timoteo2@gmail.com"."\r\n"
-    //             ."Reply-To:".$email. "\r\n"
-    //             ."X=Mailer:PHP/".phpversion();
+    $header = "From:christianfelix.timoteo2@gmail.com"."\r\n"
+                ."Reply-To:".$email. "\r\n"
+                ."X=Mailer:PHP/".phpversion();
 
-    // if (mail($to, $subject, $body,$header)){
-    //     echo ("Obrigado por entrar em contato! Seu formulário foi enviado com sucesso.");
-    // }else{
-    //     echo ("Houve um erro ao enviar sua mensagem");
-    // };
+    if (mail($to, $subject, $body,$header)){
+        echo ("Obrigado por entrar em contato! Seu formulário foi enviado com sucesso.");
+    }else{
+        echo ("Houve um erro ao enviar sua mensagem");
+    };
+
+ ?>
 
     
 
